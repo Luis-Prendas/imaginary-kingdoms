8276226b92c56362d74231143ff97f3362275fc7
LuisPrendas <luis.prendas@moonhicr.com>
2024-09-19 12:30:26 -0700

    format

--- src/providers/root-provider.tsx
+++ src/providers/root-provider.tsx
@@ -10,7 +10,7 @@ export default function RootProvider({
     <SessionProvider>
       <ThemeProvider
         attribute='class'
-        defaultTheme='system'
+        defaultTheme='dark'
         enableSystem
         disableTransitionOnChange
       >
